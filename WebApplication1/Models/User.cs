9598378b893027879e1c8b61d9aed95c61aad886
bagdadstyle <mau20410@gmail.com>
2023-06-20 20:44:37 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "El teléfono es obligatorio")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "El celular es obligatorio")]
+        public string CellPhone { get; set; }
+
+        [Required(ErrorMessage = "El email es obligatorio")]
+        public string Email { get; set; }
+    }
+}
